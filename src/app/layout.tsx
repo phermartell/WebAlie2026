a8d02c401f0e14c987b42def838d613be91faa41
phermartell <pher@aliedigital.com>
2026-08-18 01:51:14 -0600

    perf: change GTM script loading strategy to afterInteractive to prevent render blocking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,7 +80,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col bg-oled text-starlight selection:bg-orangeleader selection:text-white">
         {gtmId && (
           <>
-            <Script id="gtm-head" strategy="beforeInteractive">
+            <Script id="gtm-head" strategy="afterInteractive">
               {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${gtmId}');`}
             </Script>
             <noscript>
